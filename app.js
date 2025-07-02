916401d82b598bb246199d388a34f56b97bc1d54
sans-creator <sanskarj163@gmail.com>
2025-07-02 12:42:26 +0530

    2nd Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express= require('express');
+const app = express();
+const path = require('path');
+const { use } = require('react');
+const port=8080; // Default port
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+    console.log('Received a request for the root path');
+    //res.send('Hello, World!');
+    res.render("home")
+});
+
+app.get("/ig/:username", (req, res) => {
+    let {username} = req.params;
+    const instaData=require("./data.json");
+    console.log(instaData);
+    res.render("instagram.ejs",{data:instaData[username]});
+});
+ 
+app.get("/hello", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/rolldice", (req, res) => {
+    let diceVal=Math.floor(Math.random() * 6) + 1
+    res.render("rolldice",{num: diceVal});
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
